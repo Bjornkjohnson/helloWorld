b40ba664516f314123f2cbf096e0fe61507783ca
bjornjohnson <bjornkjohnson@gmail.com>
2016-01-05 09:04:59 -0500

    change test to validate output from stdout

--- src/com/example/helloworld/HelloWorldTest.java
+++ src/com/example/helloworld/HelloWorldTest.java
@@ -1,6 +1,12 @@
 package com.example.helloworld;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 import static com.example.helloworld.HelloWorld.*;
 import static org.junit.Assert.*;
@@ -10,12 +16,26 @@ import static org.junit.Assert.*;
  */
 public class HelloWorldTest {
 
-    String message = "Hello World";
-    HelloWorld newPrinter = new HelloWorld(message);
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private HelloWorld newPrinter;
+    String message = "Hello World!";
+
+
+    @Before
+    public void setUp() throws UnsupportedEncodingException {
+        newPrinter = new HelloWorld(message);
+        System.setOut(new PrintStream(output, true, "UTF-8"));
+    }
 
     @Test
     public void testPrintMessage() {
         assertEquals(message,newPrinter.printMessage());
+    }
+
+    @After
+    public void cleanUp() {
+        System.setOut(stdout);
     }
 }
 
